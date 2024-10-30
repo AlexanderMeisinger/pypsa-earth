ccc0eb62520617dc734801f31f3a107c91732d12
= <=>
2024-10-30 02:50:05 -0700

    Remove double carrier

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -245,7 +245,8 @@ def H2_liquid_fossil_conversions(n, costs):
 def add_hydrogen(n, costs):
     "function to add hydrogen as an energy carrier with its conversion technologies from and to AC"
 
-    n.add("Carrier", "H2")
+    #n.add("Carrier", "H2")
+
 
     n.madd(
         "Bus",
@@ -1088,7 +1089,7 @@ def add_aviation(n, cost):
 
 def add_storage(n, costs):
     "function to add the different types of storage systems"
-    n.add("Carrier", "battery")
+    #n.add("Carrier", "battery")
 
     n.madd(
         "Bus",
